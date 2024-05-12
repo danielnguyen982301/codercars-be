b37dc76d27cc415d1456dbcb0a6cec94507acd30
danielnguyen982301 <danielnguyen982301@gmail.com>
2024-05-12 16:00:32 +0700

    Modified

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to MONGODB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to Database!"));
+mongoose.connect(process.env.MONGO_URI, () =>
+  console.log("Connected to Database!")
+);
 
 app.use("/", indexRouter);
 
